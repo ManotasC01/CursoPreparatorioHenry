6bf91ef131c72cd409951dbee93577de64b6c548
ManotasC01 <manotasreyes01@gmail.com>
2023-03-06 01:03:58 -0500

    operadores

---
+++ controlDeFLujos.js
@@ -0,0 +1,11 @@
+
+
+function PuedeManejar (edad) {
+    if (edad >= 18) {
+        console.log(true);
+    } else if (edad = 17) {
+        console.log(false);
+    }
+
+}
+PuedeManejar (20);
---
+++ operadoresLogicos.js
@@ -0,0 +1,9 @@
+function mayorYMenorQue (num) {
+
+    if (num > 2 && num < 5) { console.log(true);
+    } else if (num) {console.log(false);}
+           
+    
+}
+
+mayorYMenorQue(3); 
